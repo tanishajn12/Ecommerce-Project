1e440b22d48a7d74667da8135171beefd395bfca
Tanisha Jain <tanishajn12@gmail.com>
2024-06-04 19:52:29 +0530

    New Codes

--- routes/product.js
+++ routes/product.js
@@ -1,58 +1,104 @@
 const express=  require("express");
 const Product = require('../models/Product');
+const {validateProduct} = require('../middleware');
 //mini application
 const router = express.Router(); //same work as app
 
 //read
 router.get("/products", async (req,res)=> {
-    let products = await Product.find({});
-    res.render("index",{products})
+    try{
+        let products = await Product.find({});
+        res.render("index",{products})
+    }
+
+    catch(e) {
+        res.render('error', {err : e.message})
+    }
 });
 
 //new form
 router.get("/product/new",(req,res)=>{
-    res.render('new.ejs');
+    try{
+        res.render('new.ejs'); 
+    }
+
+    catch(e) {
+        res.render('error', {err : e.message})
+    }
+   
 })
 
 //adding 
-router.post('/products', async(req,res)=>{
-    let {name, img, price, desc} = req.body; //by default undefined
-    await Product.create({name, img, price, desc}); //automatically save in db as well
-    res.redirect('/products');
+//validate product -> first check and then add
+router.post('/products', validateProduct, async(req,res)=>{
+    try{
+        let {name, img, price, desc} = req.body; //by default undefined
+        await Product.create({name, img, price, desc}); //automatically save in db as well
+        res.redirect('/products');
+    }
+    catch(e) {
+        res.render('error', {err : e.message})
+    }
 })
 
 //show particular product
 router.get('/products/:id', async (req,res)=>{
-    let {id} = req.params;
-    // let foundProduct = await Product.findById(id);
-    let foundProduct = await Product.findById(id).populate('reviews');
-    // console.log(foundProduct);
-    res.render('show',{foundProduct});
+    try{
+        let {id} = req.params;
+        // let foundProduct = await Product.findById(id);
+        let foundProduct = await Product.findById(id).populate('reviews');
+        // console.log(foundProduct);
+        res.render('show',{foundProduct});
+    }
+
+    catch(e) {
+        res.render('error', {err : e.message})
+    }
 })
 
 //show edit form
 router.get('/products/:id/edit',async (req,res)=> {
-    let {id} = req.params;
-    let foundProduct = await Product.findById(id);
-    res.render('edit',{foundProduct})
+    try{
+        let {id} = req.params;
+        let foundProduct = await Product.findById(id);
+        res.render('edit',{foundProduct})
+    }
+
+    catch(e) {
+        res.render('error', {err : e.message})
+    }
 })
 
 //actually changing the product
-router.patch('/products/:id', async(req,res)=>{
-    let {id} = req.params;
-    let {name, img, price, desc} = req.body;
-    await Product.findByIdAndUpdate(id, {name, img, price, desc});
-    res.redirect('/products')
+router.patch('/products/:id', validateProduct ,async(req,res)=>{
+    try{
+        let {id} = req.params;
+        let {name, img, price, desc} = req.body;
+        await Product.findByIdAndUpdate(id, {name, img, price, desc});
+        res.redirect('/products')
+    }
+
+    catch(e) {
+        res.render('error', {err : e.message})
+    }
 })
 
 router.delete('/products/:id', async (req,res)=>{
-    let {id} = req.params;
-    let foundProduct = await Product.findById(id);
-    for(let ids of foundProduct.reviews) {
-        await Review.findByIdAndDelete(ids);
+    try{
+        let {id} = req.params;
+        let foundProduct = await Product.findById(id);
+        //deleting reviews before deleting products
+        for(let ids of foundProduct.reviews) {
+            await Review.findByIdAndDelete(ids);
+        }
+        await Product.findByIdAndDelete(id);
+        res.redirect('/products');
     }
-    await Product.findByIdAndDelete(id);
-    res.redirect('/products');
+
+    catch(e) {
+        res.render('error', {err : e.message})
+    }
 })
 
 module.exports = router;
+
--- routes/review.js
+++ routes/review.js
@@ -4,18 +4,24 @@ const Product = require("../models/Product");
 const Review = require("../models/Review");
 
 router.post('/products/:id/rating', async (req,res)=>{
-    let {rating, comment} = req.body;
-    let {id} = req.params;
+    try{
+        let {rating, comment} = req.body;
+        let {id} = req.params;
 
-    let product = await Product.findById(id);
+        let product = await Product.findById(id);
 
-    //new review
-    let review = new Review({rating, comment});
-    product.reviews.push(review);
-    //save method is from mongo db hence awaut method
-    await product.save();
-    await review.save(); 
-    res.redirect(`/products/${id}`);
+        //new review
+        let review = new Review({rating, comment});
+        product.reviews.push(review);
+        //save method is from mongo db hence awaut method
+        await product.save();
+        await review.save(); 
+        res.redirect(`/products/${id}`);
+    }
+
+    catch(e) {
+        res.render('error',{err: e.message})
+    }
 })
 
 module.exports= router;
